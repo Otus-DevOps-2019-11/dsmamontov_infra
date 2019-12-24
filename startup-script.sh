bcaa2f76c1db46712affd65319cf757a69b810fd
Dmitriy Mamontov <d.s.mamontov@ya.ru>
2019-12-24 21:55:30 +0300

    another pipeline.. v2

--- startup-script.sh
+++ startup-script.sh
@@ -15,6 +15,12 @@ apt update && apt install -y ruby-full ruby-bundler build-essential
 
 ###
 
-runuser -l "appuser" -c "git clone -b monolith https://github.com/express42/reddit.git /home/appuser/reddit"
-runuser -l "appuser" -c "cd ~appuser/reddit && bundle install"
-runuser -l "appuser" -c "puma -d --dir /home/appuser/reddit/"
+
+sudo apt install -y git
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+puma -d
+
+#runuser -l "appuser" -c "git clone -b monolith https://github.com/express42/reddit.git /home/appuser/reddit"
+#runuser -l "appuser" -c "cd ~appuser/reddit && bundle install"
+#runuser -l "appuser" -c "puma -d --dir /home/appuser/reddit/"
