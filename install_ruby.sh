8ddf1c2f4d52304ccb0c890e6518f22870ef4a23
Dmitriy Mamontov <d.s.mamontov@ya.ru>
2019-12-24 22:09:28 +0300

    another pipeline v3

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-apt update && apt install -y ruby-full ruby-bundler build-essential
+#apt update && apt install -y ruby-full ruby-bundler build-essential
+
+sudo apt-get update
+sudo apt install -y ruby-full ruby-bundler build-essential
--- startup-script.sh
+++ startup-script.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
-###
+#wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+#sudo apt-get update
+#sudo apt-get install -y mongodb-org
+#systemctl start mongod
+#systemctl enable mongod
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-systemctl start mongod
-systemctl enable mongod
+#apt update && apt install -y ruby-full ruby-bundler build-essential
 
-###
+#sudo apt-get update
+#sudo apt install -y ruby-full ruby-bundler build-essential
 
-apt update && apt install -y ruby-full ruby-bundler build-essential
-
-###
-
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+sudo apt update
+sudo apt install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
 
 sudo apt install -y git
 git clone -b monolith https://github.com/express42/reddit.git
