c7e1cffb42111cf91a9a0ae99e59fb9816b37159
Dmitriy Mamontov <d.s.mamontov@ya.ru>
2019-12-24 21:23:26 +0300

    fix version mongo and change ip

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # dsmamontov_infra
 dsmamontov Infra repository
-
-testapp_IP = 35.222.48.51
+```
+testapp_IP = 35.224.64.222 
 testapp_port = 9292
 
 
 gcloud compute instances create reddit-app --boot-disk-size=10GB --image-family ubuntu-1604-lts --image-project=ubuntu-os-cloud --machine-type=g1-small --tags puma-server --restart-on-failure --metadata-from-file startup-script=startup-script.sh
 
 gcloud compute firewall-rules create default-puma-server --allow tcp:9292 --source-tags=puma-server --source-ranges=0.0.0.0/0 
+```
--- startup-script.sh
+++ startup-script.sh
@@ -2,8 +2,8 @@
 
 ###
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.0.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+wget -qO - https://www.mongodb.org/static/pgp/server-3.6.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 systemctl start mongod
