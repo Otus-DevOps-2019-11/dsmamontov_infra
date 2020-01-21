b96b2c67ac2719be9a25473d69bec16c423e81de
Dmitriy Mamontov <d.s.mamontov@ya.ru>
2020-01-21 21:46:15 +0300

    dome ansible

--- terraform/modules/app/files/deploy.sh
+++ terraform/modules/app/files/deploy.sh
@@ -6,7 +6,7 @@ APP_DIR=${1:-$HOME}
 git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
 cd $APP_DIR/reddit
 
-setenv DATABASE_URL "10.132.15.218:27017"
+#setenv DATABASE_URL "10.132.15.218:27017"
 
 bundle install
 
